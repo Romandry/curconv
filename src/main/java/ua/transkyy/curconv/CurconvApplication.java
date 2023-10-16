3c00f787bbc2b1e4582764daa9aa6629c152944b
Andriy <transkyy@gmail.com>
2023-10-16 16:01:46 +0300

    Initial commit

---
+++ src/main/java/ua/transkyy/curconv/CurconvApplication.java
@@ -0,0 +1,13 @@
+package ua.transkyy.curconv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurconvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurconvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/transkyy/curconv/CurconvApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.transkyy.curconv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurconvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
